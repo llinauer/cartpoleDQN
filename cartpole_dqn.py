fd8959d9e89af99af66ebc5b71fe793580d76d62
llinauer <l.linauer@gmx.at>
2023-07-03 09:48:09 +0200

    Create cartpole_dqn.py file

---
+++ cartpole_dqn.py
@@ -0,0 +1,15 @@
+"""
+
+cartpole_dqn.py
+
+Control the Cartpole environment with the DQN-method and a MLP.
+
+"""
+
+import torch
+import torch.nn as nn
+
+
+def main():
+    """ main function """
+    pass
