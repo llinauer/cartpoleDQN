bd931cbb0b08a5ea010b9240c0e478a3e0595c0c
llinauer <l.linauer@gmx.at>
2023-10-22 17:35:41 +0200

    Add MLP class

--- cartpole_dqn.py
+++ cartpole_dqn.py
@@ -8,6 +8,29 @@ Control the Cartpole environment with the DQN-method and a MLP.
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+class MLPController(nn.Module):
+    """ MLP controller with one hidden layer and tanh activation """
+
+    def __init__(self):
+        """ Constructor, define layers """
+        super().__init__()
+
+        # define layers
+        self.fc1 = nn.Linear(4, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 2)
+
+    def forward(self, x):
+        """ Forward pass through the network """
+
+        x = F.tanh(self.fc1(x))
+        x = F.tanh(self.fc2(x))
+        x = self.fc1(x)
+
+        return x
+
 
 
 def main():
