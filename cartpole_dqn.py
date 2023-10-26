3a50f89eaf36c76a21dd8f8d304fa22ac2f28606
llinauer <l.linauer@gmx.at>
2023-10-26 17:53:13 +0200

    Modify DQN and add environment_step function

--- cartpole_dqn.py
+++ cartpole_dqn.py
@@ -13,7 +13,6 @@ import gymnasium as gym
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 import custom_cartpole
 
@@ -37,14 +36,14 @@ class DQN(nn.Module):
         # define layers
         self.fc1 = nn.Linear(4, 128)
         self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, 1)
+        self.fc3 = nn.Linear(128, 2)
 
     def forward(self, x):
         """ Forward pass through the network """
 
-        x = F.tanh(self.fc1(x))
-        x = F.tanh(self.fc2(x))
-        x = self.fc1(x)
+        x = torch.tanh(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
+        x = self.fc3(x)
 
         return x
 
@@ -62,7 +61,8 @@ def parse_args():
 
 
 def sample(replay_buffer, batch_size):
-    """ Sample a batch of (state, action, next_state, reward) tuples from the replay buffer """
+    """ Sample a batch of (state, action, next_state, reward) tuples from the replay buffer.
+        Return the states, actions, rewards, dones and next_states as np.arrays"""
 
     # choose batch_size random indices from the replay buffer
     indices = np.random.choice(len(replay_buffer), batch_size, replace=False)
@@ -72,6 +72,23 @@ def sample(replay_buffer, batch_size):
     # return the states, actions, next_states
     return np.array(states), np.array(actions), np.array(rewards, dtype=np.float32), np.array(dones, dtype=np.uint8), \
         np.array(next_states)
+
+
+def environment_step(env, current_state, net, epsilon):
+    """ Do one step in the environment.
+        Choose the action either randomly or sample the best action according to the network depending on epsilon.
+        Return the taken action, the reward, if the environment is done or truncated and the next state """
+
+    # create a random number between 0 and 1, if it is > epsilon, take a random action
+    if np.random.uniform() > epsilon:
+        action = env.action_space.sample()
+    else:
+        q_vals = net(torch.tensor(current_state))
+        action = torch.argmax(q_vals).item()
+
+    # do an environment step
+    next_state, reward, done, trunc, _ = env.step(action)
+    return action, reward, done or trunc, next_state
 
 
 def main():
@@ -95,13 +112,22 @@ def main():
     # training loop
     frame_idx = 0
     epsilon = EPSILON_START
+    state, _ = env.reset()
+    optimizer = torch.optim.adam(train_net.parameters(), lr=LEARNING_RATE)
+    total_reward = 0
+    mean_reward_100 = 0.
 
     while True:
+
+        frame_idx += 1
+
         # decrease epsilon every step until EPSILON_FINAL is reached
         epsilon -= EPSILON_DECAY_STEP
         epsilon = max(epsilon, EPSILON_FINAL)
 
         # do one step in the environment
+        action, reward, done_or_trunc, next_state = environment_step(env, state, train_net, epsilon)
+
 
 
 if __name__ == '__main__':
